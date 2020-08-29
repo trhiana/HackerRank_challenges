52e876b3cd1a2f01736fae2800088650cc86fa0b
rhianathelemaque <trhiana@rocketmail.com>
2020-08-29 18:57:34 +0300

    Create Birthday Cake Candles.py

---
+++ Practice/Easy/Birthday Cake Candles.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the birthdayCakeCandles function below.
+def birthdayCakeCandles(ar):
+    count = 0
+    length = len(ar)
+    max_val = max(ar)
+    for i in range(length):
+        if ar[i] == max_val:
+            count += 1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
